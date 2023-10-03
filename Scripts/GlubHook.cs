f6ce7068770f5d99e37793838e7de2a7c36306da
Bretterbear <brettney40hands@gmail.com>
2023-10-03 16:03:52 -0700

    Quick & Dirty GlubHook
    Updates to glubhook & player to try and have some cohesiveness. Please note this is rough as heck & I intend to fix all the cruftiness.
    
    -All input grabbing takes place in player
    -aim mode is engaged via space bar (no more jump)
    -shoot is w/ left mouse button
    -on a valid connect, can warp w/ up or disconnect w/ down.
    
    Still need to make warp snapping go to valid grid points.

--- Scripts/GlubHook.cs
+++ Scripts/GlubHook.cs
@@ -1,71 +1,118 @@
 using Godot;
 using System;
-using System.Diagnostics;
 
 public partial class GlubHook : Node2D
 {
 	// ---------- Editor Variable Declarations ---------- //
-	[Export] private float _hookLength = 300f;  // Controls hook length, this will become a variable of glub count later
-	//[Export] private
+	[Export] private float _hookLength = 500f;  // Controls hook length, this will become a variable of glub count later
+	//[Export] float _senseBulbLength = 10f;	// Gives distance on a short raycast cross to grab UDLR boxed colliders if need be
 
     // -------- Non-Editor Variable Declarations -------- //
-    private Player	_player;					// Stores a reference to player for hook updating
-	private bool	_isActiveGrapple;			// Stores hook state
-	private Vector2 _grappleHookPoint;			// Stores hook location
+    //private Player	      _player;	// Stores a reference to the player for hook updating - MIGHT CUT, CURRENTLY UNNECESSARY
+    private Line2D       _grappleLine;  // Stores a reference to our line for the glub grappler
+	private Line2D    _lineAimCurrent;  // Stores a reference to our current aim line for visualizer
+    private Line2D        _lineAimMax;  // Stores a reference to our maximum range line for visualizer
+    private RayCast2D        _rayCast;  // Stores a reference to our scanning raycast
+    private bool	 _inActiveGrapple;	// Stores hook state - hooked to a target or not
+	private bool		   _inAimMode;	// Stores whether we are in "aim mode"
+	private Vector2 _grappleHookPoint;	// Stores hook location - set to zero vector unless we're currently grappling
 
-    // Called when the node enters the scene tree for the first time.
+    /// <summary>
+	/// Links Glubhook to the other nodes it needs to poke
+	/// </summary>
     public override void _Ready()
 	{
-		_player = GetParent<Player>();	// Set link to parent for communication
+		//_player = GetParent<Player>();  // Set link to parent for communication - MIGHT CUT, CURRENTLY UNNECESSARY
+		_grappleLine = GetNode<Line2D>("GrappleLine_Tmp");
+		_lineAimCurrent = GetNode<Line2D>("Line2D_AimCurr");
+        _lineAimMax = GetNode<Line2D>("Line2D_GhostAim");
+        _rayCast = GetNode<RayCast2D>("Targetter_01");
 	}
-
-    // Raycast event is called inside physics b/c raycasting space can be locked during the _input process
-    public override void _PhysicsProcess(double delta)
-    {
-		if (Input.IsActionJustPressed("action_fire"))
-		{
-			// Brett-CONSIDER adding vfx / sfx feedback on click call, esp since FireHook might fail, or may shoot ahead of / past the click point
-
-			// Ignore input if we've already got an active grapple going
-			if (!_isActiveGrapple)
-			{
-				FireHook();
-			}
-		}
-    }
 
     /// <summary>
     /// Tries to fire a grappling hook from player to mouse click position
+	/// Need to modify the IsColliding() to give a proper snap to correct positions
     /// </summary>
-    /// <returns>Whether a grapple has successfully found a target </returns>
-    public bool FireHook()
+    /// <returns>bool Whether a grapple has successfully found a target or not</returns>
+    public bool FireHook(Vector2 targetPoint)
 	{
-		DisengageHook();    // This shouldn't ever actually have to set a state, but just-in-case!
+        DisengageHook();    // Here to ensure that the hook isn't engaged when we start trying to fire
+		_rayCast.TargetPosition = targetPoint.Normalized() * _hookLength;
+		_rayCast.ForceRaycastUpdate();
 
-		// need to add the commented out code after testing
-		Vector2 targetPosition = GetViewport().GetMousePosition();
-
-        // Room for expansion - a vfx ping on mouse pin location, and a ghostly / desat raycast at length if miss
-
-        // Do a raycast to see if we hit a physics object in range
-        // Brett-CONSIDER maybe we don't want to be able to grapple down? Or maybe we do? For now ignore
-        var spaceState = GetWorld2D().DirectSpaceState;
-        var query = PhysicsRayQueryParameters2D.Create(this.GlobalPosition, targetPosition, _player.CollisionMask, new Godot.Collections.Array<Rid> { _player.GetRid() });
-		var result = spaceState.IntersectRay(query);
-
-		spaceState.IntersectRay
-
-		if (result != null)
+        if (_rayCast.IsColliding())
 		{
-			//_isActiveGrapple = true;
-			Debug.WriteLine(result.ToString());
+			// Will likely need to change this to a Vector2 Snapped() GetCollisionNormal type of thing
+			// due to the grid-snap nature of how we're gonna go in 8 directions
+			_grappleHookPoint = _rayCast.GetCollisionPoint();
+            _grappleLine.AddPoint(Vector2.Zero);
+            _grappleLine.AddPoint(ToLocal(_grappleHookPoint));
+			_inActiveGrapple = true;
+            return true;
+        }
+        else
+        {
+			// Include some sort of vfx/sfx feedback for a failed shot
+			return false;
+        }
+    }
+
+	/// <summary>
+	/// Toggles aim mode for the glub hook + aim visualizer
+	/// </summary>
+	public void ToggleAimVisualizer()
+	{
+        _inAimMode = !_inAimMode;
+
+        if (_inAimMode)									// Set up line points for aim visual aid
+		{
+			for (int i = 0; i < 2; i++)
+			{
+                _lineAimCurrent.AddPoint(Vector2.Zero);	
+				_lineAimCurrent.AddPoint(Vector2.Zero); // Line is only duplicated b/c I'm using 4pts for the current aim line
+                _lineAimMax.AddPoint(Vector2.Zero);
+            }
+		}
+		else											// Break down aim lines & disengage hook just in case
+		{
+			_lineAimCurrent.ClearPoints();
+			_lineAimMax.ClearPoints();
+            GetNode<Label>("../Label").Text = "";		// Delete this line if you get rid of the debugVisualize display below (and "Label" node in prefab)
+			DisengageHook();
+        }
+	}
+
+	/// <summary>
+	/// Creates a visual display of reachable tiles by grappling hook
+	/// </summary>
+	public void VisualizeAim(Vector2 targetPoint)
+	{
+		// Local scope variables for behavior manipulation
+		float aimTBarSize = 10f;    // Sets the size of the aiming "crosshair" for aim path
+		bool debugDisplay = true;   // TMP; whether to display aim info on screen, if you get rid of this, kill the label on the player prefab
+
+		// Set max aimline end point to furthest distance along vurrent point direciton
+		_lineAimMax.SetPointPosition(1, targetPoint.Normalized() * _hookLength);
+
+		// Current aim length cannot exceed maximum reach
+		if (targetPoint.Length() > _hookLength)
+		{
+			targetPoint = _lineAimMax.GetPointPosition(1);
 		}
 
-        // if raycast hit in range so, set a vector2 w/ grapple location
+		_lineAimCurrent.SetPointPosition(1, targetPoint);
 
+		// Forms a little "t-bar" at the current aim point (just for ease of sight)
+		Vector2 headOffsetVector = targetPoint.Orthogonal().Normalized();
+		_lineAimCurrent.SetPointPosition(2, targetPoint + headOffsetVector * aimTBarSize);
+		_lineAimCurrent.SetPointPosition(3, targetPoint + headOffsetVector * -aimTBarSize);
 
-        Debug.WriteLine("FIRED HOOK");
-        return true;
+		// Delete the below code after you've gotten the hook functioning how you want!
+		if (debugDisplay)
+		{
+            Label Label = GetNode<Label>("../Label");
+            Label.Text = "target pos - " + targetPoint.ToString() + "\nlength - " + (Math.Round(targetPoint.Length(), 2).ToString());
+        }
     }
 
 	/// <summary>
@@ -73,8 +120,24 @@ public partial class GlubHook : Node2D
 	/// </summary>
 	public void DisengageHook()
 	{
-		_isActiveGrapple = false;			// Reset grapple state
-		_grappleHookPoint = Vector2.Zero;	// Reset grapple hook point
+		_inActiveGrapple = false;			// Reset grapple state
+		_grappleHookPoint = Vector2.Zero;   // Reset grapple hook point
+		_grappleLine.ClearPoints();			// Resets the grapple line to be non-existant again
+	}
 
+	/// <summary>
+	/// Getter for glubHook's understanding of whether we're in a grapple right now
+	/// </summary>
+	public bool IsInGrapple()
+	{
+		return _inActiveGrapple;
+	}
+
+	/// <summary>
+	/// Gives back where the grappling hook has been lodged
+	/// </summary>
+	public Vector2 GetHookPoint()
+	{
+		return _grappleHookPoint;
 	}
 }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,40 +1,154 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 800.0f;
-	public const float JumpVelocity = -400.0f;
+    // ---------- Editor Variable Declarations ---------- //
+    [Export] private const float _Speed = 500.0f;                // Will need to fine tune, but controls player horizontal speed
+    [Export] private Vector2 _stepSize = new Vector2(64, 64);    // Stores our grid step size
 
-	// Get the gravity from the project settings to be synced with RigidBody nodes.
-	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    // -------- Non-Editor Variable Declarations -------- //
+    private float      gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    private bool    _inAimMode;     // BH - Likely replace this w/ a playerstate enum later
+    private Vector2 _dPadInput;     // Stores current dPadInput
+    private GlubHook  glubHook;     // Stores a reference to our glub hook for function calling
 
-	public override void _PhysicsProcess(double delta)
-	{
-		Vector2 velocity = Velocity;
+    /// <summary>
+    /// As of now all we're doing is setting our glubHook reference here
+    /// </summary>
+    public override void _Ready()
+    {
+        glubHook = GetNode<GlubHook>("GlubHook");   // We will heavily use this glubHook reference
+        _inAimMode = false;                         // We don't start out aimed
+    }
 
-		// Add the gravity.
-		if (!IsOnFloor())
-			velocity.Y += gravity * (float)delta;
+    /// <summary>
+    /// We're going to tweak this to basically be an enum switch func call to the various player motion states / types
+    /// </summary>
+    public override void _PhysicsProcess(double delta)
+    {
+        AimModeCheckAndUpdate();
+        SnagDPadInput();
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("move_jump") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+        if (_inAimMode)
+        {
+            HandleAimMode(delta);
+        }
+        else
+        {
+            HandleFreeMode(delta);
+        }
+    }
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down" +
-			"");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+    /// <summary>
+    /// Checks for mode_toggle_aim input and sets the mode accordingly in Player + GlubHook
+    /// </summary>
+    private void AimModeCheckAndUpdate()
+    {
+        if (Input.IsActionJustPressed("mode_toggle_aim"))
+        {
+            if (_inAimMode)
+            {
+                _inAimMode = false;
+                glubHook.ToggleAimVisualizer();
+            }
+            else
+            {
+                _inAimMode = true;
+                this.Position = this.Position.Snapped(_stepSize);    // Need to hide this snap w/ a smooth motion + a little vfx dazzle puff
+                Velocity = Vector2.Zero;
+                glubHook.ToggleAimVisualizer();
+            }
+        }
 
-		Velocity = velocity;
-		MoveAndSlide();
-	}
+        if (_inAimMode)
+        {
+            glubHook.VisualizeAim(GetLocalMousePosition());
+        }
+    }
+
+    /// <summary>
+    /// Handling for logic branch where you're in "aim/shoot" mode
+    /// delta currently unused, likely will be
+    /// </summary>
+    private void HandleAimMode(double delta)
+    {
+        if (Input.IsActionJustPressed("action_fire"))
+        {
+            if (glubHook.FireHook(GetLocalMousePosition()))
+            {
+                // Replace this w/ success SFX/VFX caLL
+            }
+            else
+            {
+                // Replace this w/ fail SFX/VFX call
+            }
+        }
+        else
+        {
+            if (glubHook.IsInGrapple())
+            {
+                if (_dPadInput.Y < 0)
+                {
+                    // Replace this w/ a call to an ienumerator that makes a smooth motion as opposed to a hop + add input disabling mid smoothmove
+                    this.Position = glubHook.GetHookPoint().Snapped(_stepSize);
+                    glubHook.DisengageHook();
+                    // Need to add a ground check for autodisengage if you're on the ground, made somewhat stickier by "snapped" grid positioning
+                }
+                else if (_dPadInput.Y > 0)
+                {
+                    glubHook.DisengageHook();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handling branch for any player movement not under the auspices of aim mode
+    /// delta currently unused, likely will be
+    /// </summary>
+    private void HandleFreeMode(double delta)
+    {
+        Velocity = SetVelocityRegularly(delta);
+        MoveAndSlide();
+    }
+
+    /// <summary>
+    /// Some fairly basic movement code, to be made nicer later
+    /// </summary>
+    private Vector2 SetVelocityRegularly(double delta)
+    {
+        //gives us a local handle for velocity tweakery
+        Vector2 velocity = Velocity;
+
+        // Add the gravity.
+        if (!IsOnFloor())
+            velocity.Y += gravity * (float)delta;
+
+        if (_dPadInput != Vector2.Zero)
+        {
+            velocity.X = _dPadInput.X * _Speed;
+        }
+        else
+        {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, _Speed);
+        }
+
+        return velocity;
+    }
+
+    /// <summary>
+    /// Currently unused, will be used to handle smooth position changing for a grapple transition
+    /// </summary>
+    private Vector2 SetVelocityGrapple(double delta)
+    {
+        return Vector2.Zero;
+    }
+
+    /// <summary>
+    /// One line function to set _dPadInput - might grow more complex if we start adding movement magnitude, etc
+    /// </summary>
+    private void SnagDPadInput()
+    {
+        _dPadInput = Input.GetVector("move_left", "move_right", "move_up", "move_down" + "");
+    }
 }
