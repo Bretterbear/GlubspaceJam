e4361143a7454eb22964af1bb68640a310ae0bbb
DigitDaemon <thomasclandry95@gmail.com>
2023-10-15 16:32:51 -0700

    Updates

--- Scripts/InvertedSpikeAreaController.cs
+++ Scripts/InvertedSpikeAreaController.cs
@@ -1,24 +1,42 @@
 using Godot;
 using System;
+using System.Diagnostics;
+using GlubspaceJam.Scripts;
 
-public partial class InvertedSpikeAreaController : Area2D
+public partial class InvertedSpikeAreaController : Area2D, IDynamicReceiver
 {
 	private bool _powered;
+
+	private bool _intialized;
 	// Called when the node enters the scene tree for the first time.
 
 	public override void _Ready()
 	{
-		ToggleSpikes();
+		
 	}
 
+	public override void _PhysicsProcess(double delta)
+	{
+		while (!_intialized)
+		{
+			var collisions = GetOverlappingAreas().Count;
+			if (collisions > 0)
+			{
+				_intialized = true;
+				ToggleSpikes();
+			}
+		}
+	}
 	public void ProvidePower()
 	{
 		_powered = true;
+		ToggleSpikes();
 	}
 
 	public void StopPower()
 	{
 		_powered = false;
+		ToggleSpikes();
 	}
 
 	public bool IsOn()
@@ -28,18 +46,20 @@ public partial class InvertedSpikeAreaController : Area2D
 
 	private void ToggleSpikes()
 	{
-		var children = GetOverlappingBodies();
+		Debug.WriteLine("ToggleSpikes");
+		var children = GetOverlappingAreas();
+		Debug.WriteLine(children.Count);
 		foreach (var child in children)
 		{
 			if (child is Spike)
 			{
-				if (!_powered)
+				if (_powered)
 				{
-					((Spike)child).TurnOffSpike();
+					((Spike)child).TurnOnSpike();
 				}
 				else
 				{
-					((Spike)child).TurnOnSpike();
+					((Spike)child).TurnOffSpike();
 				}
 			}
 		}
