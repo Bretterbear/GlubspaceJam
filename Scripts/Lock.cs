b59dd7c250adfcf72dae275cce04fab381a6259c
DigitDaemon <thomasclandry95@gmail.com>
2023-10-15 21:22:20 -0700

    Fixed Lock

--- Scripts/Lock.cs
+++ Scripts/Lock.cs
@@ -12,8 +12,6 @@ public partial class Lock : Sprite2D, IDynamicReceiver
 
 	private bool _inverted;
 
-	private IDynamicReceiver _dynamicReceiverImplementation;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
